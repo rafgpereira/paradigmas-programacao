bed877f5748b4095ad44267fba3c883829672684
Rafael Gomes Pereira <rafaelgomespereira123@gmail.com>
2025-08-19 10:47:22 -0300

    Update irmaos.pl

--- prolog/irmaos.pl
+++ prolog/irmaos.pl
@@ -1,1 +1,14 @@
-
+homem(homer).
+homem(bart).
+mulher(marge).
+mulher(lisa).
+mulher(maggie).
+pai(homer, bart).
+pai(homer, lisa).
+pai(homer, maggie).
+mae(marge, bart).
+mae(marge, lisa).
+mae(marge, maggie).
+irmaos(X, Y) :- X \= Y, pai(F, X), pai(F, Y).
+irmao(X, Y) :- homem(X), irmaos(X, Y).
+irma(X, Y) :- mulher(X), irmaos(X, Y).
